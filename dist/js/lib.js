aa68f886449bc44e6f73004653c1a79a7d21ac1e
srg <skabassjob@gmail.com>
2024-10-09 21:04:37 +0400

    build: update

--- dist/js/lib.js
+++ dist/js/lib.js
@@ -24,7 +24,7 @@ window.utils = {
 ;(() => {
   const accordions = document.querySelectorAll('.js-accordion')
 
-  if (!accordions) {
+  if (accordions.length === 0) {
     return
   }
 
@@ -46,6 +46,56 @@ window.utils = {
       }
     })
   })
+})()
+
+/**
+ * block-application.js
+ */
+;(() => {
+  const elements = document.querySelectorAll('.js-el')
+
+  if (elements.length === 0) {
+    return
+  }
+
+  elements.forEach((it) => {
+    const field = it.querySelector('.js-el-field')
+
+    if (!field && !field.maxLength) {
+      return
+    }
+
+    const subFieldElement = it.querySelector('.js-el-maxlength')
+
+    if (!subFieldElement) {
+      return
+    }
+
+    showRestInput(field, subFieldElement)
+
+    field.addEventListener('input', (evt) => {
+      if (evt.target.value.length > evt.target.maxLength) {
+        evt.target.value = evt.target.value.slice(0, maxLength)
+      }
+
+      showRestInput(evt.target, subFieldElement)
+    })
+  })
+
+  /**
+   * @param {HTMLInputElement | HTMLTextAreaElement } field
+   * @param {HTMLDivElement} element
+   */
+  function showRestInput(field, element) {
+    const remainder = field.maxLength - field.value.length
+    element.textContent = `осталось ${remainder} символов`
+
+    if (remainder === 0) {
+      element.classList.add('error')
+    } else {
+      element.classList.remove('error')
+    }
+  }
 })()
 
 /**
