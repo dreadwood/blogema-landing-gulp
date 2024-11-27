ff69c44d1976048e5c2ad1966ec51e595fb3f7ed
srg <skabassjob@gmail.com>
2024-11-27 20:45:11 +0400

    build: update

--- dist/js/lib.js
+++ dist/js/lib.js
@@ -36,16 +36,25 @@ window.utils = {
       return
     }
 
+    setContentHeight(it, content)
+
     btn.addEventListener('click', () => {
       it.classList.toggle('show')
-
-      if (it.classList.contains('show')) {
-        content.style = `height: ${content.scrollHeight}px;`
-      } else {
-        content.style = ''
-      }
+      setContentHeight(it, content)
     })
   })
+
+  /**
+   * @param {HTMLDivElement} accordion
+   * @param {HTMLDivElement} content
+   */
+  function setContentHeight(accordion, content) {
+    if (accordion.classList.contains('show')) {
+      content.style = `height: ${content.scrollHeight}px;`
+    } else {
+      content.style = ''
+    }
+  }
 })()
 
 /**
@@ -392,6 +401,45 @@ window.utils = {
       closeMenu()
     })
   })
+})()
+
+/**
+ * block-winners.js
+ */
+;(() => {
+  console.log('test')
+  const accordions = document.querySelectorAll('.js-winner')
+
+  console.log(accordions)
+
+  accordions.forEach((it) => {
+    const btn = it.querySelector('.js-accordion-btn')
+    const content = it.querySelector('.js-accordion-content')
+
+    if (!btn || !content) {
+      return
+    }
+
+    setContentHeight(it, content)
+
+    btn.addEventListener('click', () => {
+      it.classList.toggle('show')
+
+      setContentHeight(it, content)
+    })
+  })
+
+  /**
+   * @param {HTMLDivElement} accordion
+   * @param {HTMLDivElement} content
+   */
+  function setContentHeight(accordion, content) {
+    if (accordion.classList.contains('show')) {
+      content.style = `height: ${content.scrollHeight}px;`
+    } else {
+      content.style = ''
+    }
+  }
 })()
 
 /**
